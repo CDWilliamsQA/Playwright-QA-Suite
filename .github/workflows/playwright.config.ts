9c18b1fe2ba609283c9d82adb2e3fbcfd596141b
Christopher Williams <c_d_w2005@hotmail.co.uk>
2026-01-22 16:21:20 +0000

    Update playwright.config.ts

--- .github/workflows/playwright.config.ts
+++ .github/workflows/playwright.config.ts
@@ -1,1 +1,30 @@
-{}
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+  timeout: 30 * 1000,
+  retries: 1,
+  reporter: [['html', { open: 'never' }], ['junit', { outputFile: 'results.xml' }]],
+
+  use: {
+    baseURL: 'https://example.com',
+    trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
+  },
+
+  projects: [
+    {
+      name: 'Chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    {
+      name: 'Firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+    {
+      name: 'WebKit',
+      use: { ...devices['Desktop Safari'] },
+    },
+  ],
+});
