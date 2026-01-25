26d012fd8863eab93c78185a799677ea2db198c2
Christopher Williams <c_d_w2005@hotmail.co.uk>
2026-01-25 15:24:32 +0000

    Create api.spec.ts

---
+++ tests/api.spec.ts
@@ -0,0 +1,15 @@
+import { test, expect, request } from @'playwright/test';
+
+test.describe('API Validation', () => {
+  test('GET request to verify API status', async ({ request }) => {
+    const repsonse = await
+request.get('https://reqres.in/api/users/2');
+    expect(response.ok()).toBeTruthy():
+
+       const body = await response.json ()
+      constole.log (body));
+
+expect(body.data).toHaveProperty('email')
+    ;
+  });
+});
