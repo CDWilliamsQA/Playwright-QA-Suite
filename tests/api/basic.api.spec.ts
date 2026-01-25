6877a833ff4d49504d3698d8b7f8d1a0a969a0f2
Christopher Williams <c_d_w2005@hotmail.co.uk>
2026-01-25 17:12:36 +0000

    Update basic.api.spec.ts

--- tests/api/basic.api.spec.ts
+++ tests/api/basic.api.spec.ts
@@ -1,6 +1,9 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from
+  'playwight/test';
 
-test('API: GET / returns 200', async ({ request }) => {
-  const res = await request.get('/');
+test('API GET returns 200', aysnc
+     ({ request}) => {
+     const res = await
+request.get('https://reqres.in/api/users/2');
   expect(res.status()).toBe(200);
 });
